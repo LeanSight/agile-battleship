76089356b5e11a6a921098528f624caddcecdefd
Agustin <agustin.villena@gmail.com>
2016-09-21 13:55:03 +0000

    App ignores f5, preventing to reset the game by mistake

--- javascripts/boot.js
+++ javascripts/boot.js
@@ -1,11 +1,26 @@
 $(function() {
-  var game;
-
-  $("#newGame").bind("click", function() {
-    if (game) {
-      game.destroy();
-    }
-    game = new Game({shotsPerIteration: $("#shotsPerIteration").val()});
-    new GameView({model: game}).render();
-  });
+	// avoid that f5 resets the game
+	document.onkeydown = function(){
+	  switch (event.keyCode){
+			case 116 : //F5 button
+				event.returnValue = false;
+				event.keyCode = 0;
+				return false;
+			case 82 : //R button
+				if (event.ctrlKey){ 
+					event.returnValue = false;
+					event.keyCode = 0;
+					return false;
+				}
+		}
+	}
+	// initialize game
+	var game;
+	$("#newGame").bind("click", function() {
+		if (game) {
+			game.destroy();
+		}
+		game = new Game({shotsPerIteration: $("#shotsPerIteration").val()});
+		new GameView({model: game}).render();
+	});
 });
