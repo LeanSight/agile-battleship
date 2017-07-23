e0d6f1021d9109d538a40086db83553661a2c8b5
Agustin <agustin.villena@gmail.com>
2017-07-23 22:11:29 +0000

    ADD archivo chart.js

---
+++ javascripts/app/Chart.js
@@ -0,0 +1,25 @@
+var ChartData = {
+  "type": "line",
+  "scale-x": {
+    "values": "0:40:5"
+  },
+  "scale-y": {
+    "max-value": 900,
+    "format": "$%vk",
+    "markers": [{
+      "type": "line",
+      "range": [
+        400
+      ],
+      "line-width": 3,
+      "line-color": "green",
+      "text": "Presupuesto"
+    }]
+  },
+  "series": [{
+    "values": [
+      [0, 400]
+    ],
+    "palette": 0
+  }]
+};
